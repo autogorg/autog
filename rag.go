0bc63e9849d8cd7719ac7222c415f67c83f7f644
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2024-03-21 11:43:02 +0300

    add memory database

--- rag.go
+++ rag.go
@@ -1,5 +1,31 @@
 package autog
 
+
+type MemoryDatabase interface {
+	AddDoc(doc Document)
+	GetDoc(path string)
+	DelDoc(path string)
+	GetDocChunks(path string) []Chunk
+	GetDocChunk(path string, idx int) Chunk
+	GetChunks() []Chunk
+}
+
+type Chunk struct {
+	Index     int       `json:"Index"`
+	Query     string    `json:"Query"`
+	Text      string    `json:"Text"`
+	ByteStart int       `json:"ByteStart"`
+	ByteEnd   int       `json:"ByteEnd"`
+	Embedding []float64 `json:"Embedding"`
+}
+
+type Document struct {
+	Path    string  `json:"Path"`
+	Title   string  `json:"Title"`
+	Desc    string  `json:"Desc"`
+	Chunks  []Chunk `json:"Chunks"`
+}
+
 type Rag struct {
 	Index func()
 	Retrieve func(query string) 
